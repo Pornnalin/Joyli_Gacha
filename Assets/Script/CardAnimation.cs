ebe0d4c2034ee542a1f9d9950b630f8f8338f38a
MaiSang <51067238+Pornnalin@users.noreply.github.com>
2024-05-28 13:30:39 +0700

    Update CardAnimation.cs

--- Assets/Script/CardAnimation.cs
+++ Assets/Script/CardAnimation.cs
@@ -1,3 +1,4 @@
+using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,18 @@ public class CardAnimation : MonoBehaviour
 {
     [SerializeField] private RectTransform frontCard;
     [SerializeField] private RectTransform backCard;
+    [SerializeField] private GameObject cardGroup;
     public float timeBack;
     public float timeFront;
     public AnimationCurve animationCurveBack;
     public AnimationCurve animationCurveFront;
+    [SerializeField] private RectTransform targetRect;
+    //[SerializeField] private Vector2 sizeTarget;
     // Start is called before the first frame update
     void Start()
     {
-
         RotateCardOpen();
+        //RotateCardOpenLoop();
     }
 
     // Update is called once per frame
@@ -23,7 +27,7 @@ public class CardAnimation : MonoBehaviour
     {
 
     }
-    public void RotateCardOpen()
+    public void RotateCardOpenLoop()
     {
         LeanTween.rotateLocal(backCard.gameObject, new Vector3(0, 90, 0), timeBack)
            .setEase(animationCurveBack)
@@ -50,9 +54,51 @@ public class CardAnimation : MonoBehaviour
                    .setEase(animationCurveBack).setOnComplete(() =>
                    {
 
-                       RotateCardOpen();
+                       RotateCardOpenLoop();
                    });
 
            }).setDelay(1f);
+    }
+    public void RotateCardOpen()
+    {
+        LeanTween.rotateLocal(backCard.gameObject, new Vector3(0, 90, 0), timeBack)
+           .setEase(animationCurveBack)
+           .setOnComplete(() =>
+           {
+
+               LeanTween.rotateLocal(frontCard.gameObject, Vector3.zero, timeFront)
+                   .setEase(animationCurveFront).setOnComplete(() =>
+                   {
+                       MoveCardToSlot();
+                   });
+
+           }).setDelay(1f);
+    }
+
+    public void MoveCardToSlot()
+    {
+        LeanTween.move(cardGroup, targetRect.position, 1f)
+           .setEase(LeanTweenType.easeInSine).setDelay(1f).setOnUpdate((float a) =>
+           {
+            //   LeanTween.alpha(frontCard, 0.3f, 0.1f).setEase(LeanTweenType.easeOutQuint);
+
+           });
+
+        LeanTween.size(frontCard, new Vector2(frontCard.rect.width / 3, frontCard.rect.height / 3), 1f).setEase(LeanTweenType.easeInSine).setDelay(1f).setOnComplete(() =>
+              {
+               //   LeanTween.alpha(frontCard, 1f, 1f).setEase(LeanTweenType.easeInSine);
+
+              });
+
+
+
+
+    }
+    [ContextMenu("debug")]
+    public void DebugSize()
+    {
+        Debug.Log(targetRect.sizeDelta.x);
+        Debug.Log(targetRect.sizeDelta.y);
+        Debug.Log(frontCard.sizeDelta);
     }
 }
